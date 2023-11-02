325b1339019c170d6490023330ace62f9fa4a85d
Luiz Henrique Ferreira <luizhff@gmail.com>
2023-11-01 21:04:29 -0300

    feat: create sticket from image or url

---
+++ src/commands/sticket.ts
@@ -0,0 +1,40 @@
+import { ICommand, QuoteMessage } from '../interfaces';
+import axios from 'axios';
+import { Client, Message, MessageMedia } from 'whatsapp-web.js';
+
+class SticketCommand implements ICommand {
+  readonly name = '/sticket';
+
+  constructor() {}
+
+  async handle(client: Client, message: Message): Promise<void> {
+    if (message.type === 'image') {
+      const image = await message.downloadMedia();
+      const media = await new MessageMedia(image.mimetype, image.data);
+
+      client.sendMessage(message.from, media, {
+        sendMediaAsSticker: true,
+        stickerAuthor: 'wa-sticker-bot:LhuizF',
+        stickerName: 'sticker',
+      });
+
+      return;
+    }
+    const [, url] = message.body.split(' ');
+
+    const { data } = await axios
+      .get(url.trim(), { responseType: 'arraybuffer' })
+      .catch((err) => {
+        console.log('ERROR', err.response.data);
+        client.sendMessage(message.from, 'Erro ao baixar imagem!');
+        throw err;
+      });
+
+    const imageBase = Buffer.from(data).toString('base64');
+    const image = await new MessageMedia('image/jpeg', imageBase, 'image.jpg');
+
+    await client.sendMessage(message.from, image, { sendMediaAsSticker: true });
+  }
+}
+
+export default SticketCommand;
